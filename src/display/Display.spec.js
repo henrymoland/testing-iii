eda8a0c6a99f8841432cf48a7e44a62dc8b35178
Henry Moland <molandh@gmail.com>
2019-02-07 07:53:54 -0800

    completed testing for controls and Dashboard

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,39 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+describe('Describe the display component', () => {
+    it('renders the display', () => {
+        render(<Display />)
+    });
+
+    it('display closed if closed property is true', () => {
+        const { getByText } = render(<Display closed= { true }/>)
+        getByText(/closed/i)
+    });
+
+    it('display open if closed property is false', () => {
+        const { getByText } = render(<Display closed= { false }/>)
+        getByText(/open/i)
+    });
+
+    it("displays 'Locked' if the locked prop is true", () => {
+        const {getByText} = render(<Display locked={true}/>);
+        getByText(/locked/i)
+
+         
+        const div = getByText(/locked/i);
+        expect(div).toHaveClass("green-led");
+    });
+
+    it("displays 'unlocked' if the closed prop is false", () => {
+        const {getByText} = render(<Display locked={false}/>);
+        getByText(/unlocked/i)  
+
+        const div = getByText(/unlocked/i);
+        expect(div).toHaveClass("green-led");   
+ });    
+});
